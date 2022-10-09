6774a92c983cbfee98592d3db923d8c74a010127
frzrbox <davidefhan@hey.com>
2022-10-08 23:53:37 -1000

    Add assets to passthrough copy

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget('src');
+  eleventyConfig.addPassthroughCopy({ 'src/assets/': 'assets' });
 
   return {
     dir: {
