759fd7172e545d620fd8a67f1723ee01fe5e689c
frzrbox <frzrbox@users.noreply.github.com>
2022-10-09 02:22:52 +0000

    deploy: 88ab4eb479b5249b78c4c2b2c41affd898d68109

--- js/elements/text-stack.js
+++ js/elements/text-stack.js
@@ -1,48 +1,48 @@
 class Stack extends HTMLElement {
-	constructor() {
-		super();
+  constructor() {
+    super();
 
-		// Create a Shadow Dom
-		this.attachShadow({ mode: "open" });
-		// Create an element
-		const template = document.querySelector("#text-stack-template");
-		const clone = template.content.cloneNode(true);
+    // Create a Shadow Dom
+    this.attachShadow({ mode: 'open' });
+    // Create an element
+    const template = document.querySelector('#text-stack-template');
+    const clone = template.content.cloneNode(true);
 
-		// Append to Shadow Dom
-		this.shadowRoot.appendChild(clone);
+    // Append to Shadow Dom
+    this.shadowRoot.appendChild(clone);
 
-		// Find element to insert into
-		this.headline = this.shadowRoot.querySelector(".text-stack__headline");
-		this.headline.innerHTML = this.headlineAttribute;
+    // Find element to insert into
+    this.headline = this.shadowRoot.querySelector('.text-stack__headline');
+    this.headline.innerHTML = this.headlineAttribute;
 
-		this.body = this.shadowRoot.querySelector(".text-stack__body");
-		this.body.innerHTML = this.bodyAttribute;
-	}
+    this.body = this.shadowRoot.querySelector('.text-stack__body');
+    this.body.innerHTML = this.bodyAttribute;
+  }
 
-	// This method is called when the element in inserted into the document
-	connectedCallback() {}
+  // This method is called when the element in inserted into the document
+  connectedCallback() {}
 
-	// This method is called when the element is removed from the document
-	disconnectedCallback() {}
+  // This method is called when the element is removed from the document
+  disconnectedCallback() {}
 
-	// Where we list attributes that should be observed
-	static get observedAttributes() {}
+  // Where we list attributes that should be observed
+  static get observedAttributes() {}
 
-	// Runs when one of the observed attributes changes
-	attributeChangedCallback(attrName, oldVal, newVal) {}
+  // Runs when one of the observed attributes changes
+  attributeChangedCallback(attrName, oldVal, newVal) {}
 
-	// Calls when the element is moved to a new document
-	// Very rarely used
-	adoptedCallback() {}
+  // Calls when the element is moved to a new document
+  // Very rarely used
+  adoptedCallback() {}
 
-	// To use an attribute you need a getter and/or setter
-	get headlineAttribute() {
-		return this.getAttribute("headline");
-	}
+  // To use an attribute you need a getter and/or setter
+  get headlineAttribute() {
+    return this.getAttribute('headline');
+  }
 
-	get bodyAttribute() {
-		return this.getAttribute("body");
-	}
+  get bodyAttribute() {
+    return this.getAttribute('body');
+  }
 }
 
-window.customElements.define("nma-text-stack", Stack);
+window.customElements.define('nma-text-stack', Stack);
