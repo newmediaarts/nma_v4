9db44b8c21c6ef4699c5208a3113531556ac114a
frzrbox <frzrbox@users.noreply.github.com>
2022-10-09 03:04:55 +0000

    deploy: bbb7d3e46b299b828ad0a5d4ebb9791965cfcbe0

--- js/main.js
+++ js/main.js
@@ -1,11 +1,41 @@
-// Custom Elements
-import './elements/text-stack.js';
+(() => {
+  // src/js/elements/text-stack.js
+  var Stack = class extends HTMLElement {
+    constructor() {
+      super();
+      this.attachShadow({ mode: "open" });
+      const template = document.querySelector("#text-stack-template");
+      const clone = template.content.cloneNode(true);
+      this.shadowRoot.appendChild(clone);
+      this.headline = this.shadowRoot.querySelector(".text-stack__headline");
+      this.headline.innerHTML = this.headlineAttribute;
+      this.body = this.shadowRoot.querySelector(".text-stack__body");
+      this.body.innerHTML = this.bodyAttribute;
+    }
+    connectedCallback() {
+    }
+    disconnectedCallback() {
+    }
+    static get observedAttributes() {
+    }
+    attributeChangedCallback(attrName, oldVal, newVal) {
+    }
+    adoptedCallback() {
+    }
+    get headlineAttribute() {
+      return this.getAttribute("headline");
+    }
+    get bodyAttribute() {
+      return this.getAttribute("body");
+    }
+  };
+  window.customElements.define("nma-text-stack", Stack);
 
-// Other app logic can go here
-class App {
-  constructor() {
-    console.log('Hello');
-  }
-}
-
-new App();
+  // src/js/main.js
+  var App = class {
+    constructor() {
+      console.log("Hello this is using esbuild");
+    }
+  };
+  new App();
+})();
