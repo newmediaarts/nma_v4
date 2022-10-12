499499b57da8bc96c4730988029ce6cc17e0b03d
frzrbox <frzrbox@users.noreply.github.com>
2022-10-12 10:32:50 +0000

    deploy: 6a961cd0d85ab7acd85dfc1c8d80c9fbf33513c8

--- js/main.js
+++ js/main.js
@@ -1,26 +1,4 @@
 (() => {
-  // src/js/elements/text-stack.js
-  var Stack = class extends HTMLElement {
-    constructor() {
-      super();
-      this.attachShadow({ mode: "open" });
-      const template = document.querySelector("#text-stack-template");
-      const clone = template.content.cloneNode(true);
-      this.shadowRoot.appendChild(clone);
-      this.headline = this.shadowRoot.querySelector(".text-stack__headline");
-      this.headline.innerHTML = this.headlineAttribute;
-      this.body = this.shadowRoot.querySelector(".text-stack__body");
-      this.body.innerHTML = this.bodyAttribute;
-    }
-    get headlineAttribute() {
-      return this.getAttribute("headline");
-    }
-    get bodyAttribute() {
-      return this.getAttribute("body");
-    }
-  };
-  window.customElements.define("nma-text-stack", Stack);
-
   // src/js/elements/picture.js
   var Picture = class extends HTMLElement {
     constructor() {
