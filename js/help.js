264d0ec3161ce7e127187075453b7cd2d9537bb7
frzrbox <frzrbox@users.noreply.github.com>
2022-10-13 04:17:23 +0000

    deploy: 6d63641b33efb78972a3839af568b0d250882e86

---
+++ js/help.js
@@ -0,0 +1,74 @@
+(() => {
+  // src/js/help.js
+  var helpTemplate = `
+  <div class="help">
+    <div class="help__options">
+      <div class="help__option>
+        <label class="help__option__label" id="grid">Toggle Grid</label>
+        <input type="checkbox" class="help__option__input" id="grid" name="grid" />
+      </div>
+    </div>
+    <button class="help__toggle" aria-label="Toggle help"></button>
+  </div>
+`;
+  document.body.innerHTML += helpTemplate;
+  var Help = class {
+    constructor() {
+      this.container = document.querySelector(".help");
+      this.showHelp = false;
+      this.showGrid = false;
+      this.helpToggleButton = this.container.querySelector(".help__toggle");
+      this.helpOptionsContainer = this.container.querySelector(".help__options");
+      this.gridCheckbox = this.container.querySelector('input[type="checkbox"]#grid');
+      this.helpToggleButton.addEventListener("click", () => {
+        this.container.classList.toggle("help--active");
+      });
+      this.gridCheckbox.addEventListener("change", () => this.toggleGrid());
+      window.addEventListener(
+        "resize",
+        debounce(() => {
+          if (this.showGrid) {
+            this.sizeGrid();
+          }
+        }, 500)
+      );
+    }
+    toggleGrid() {
+      this.showGrid = !this.showGrid;
+      if (this.showGrid) {
+        this.createGrid();
+      } else {
+        this.gridElement.remove();
+      }
+    }
+    createGrid() {
+      this.gridElement = document.createElement("div");
+      this.gridElement.classList.add("help-grid");
+      this.sizeGrid();
+      document.body.appendChild(this.gridElement);
+    }
+    sizeGrid() {
+      const columnCount = Number(getCustomProperty("--grid-column-count"));
+      const oldColumns = this.gridElement.querySelectorAll(".help-grid__column");
+      oldColumns.forEach((column) => column.remove());
+      for (let i = 0; i < columnCount; i++) {
+        const column = document.createElement("div");
+        column.classList.add("help-grid__column");
+        column.style.setProperty("--column-number", i + 1);
+        this.gridElement.appendChild(column);
+      }
+    }
+  };
+  function getCustomProperty(prop) {
+    return getComputedStyle(document.documentElement).getPropertyValue(prop);
+  }
+  function debounce(func) {
+    var timer;
+    return function(event) {
+      if (timer)
+        clearTimeout(timer);
+      timer = setTimeout(func, 100, event);
+    };
+  }
+  new Help();
+})();
